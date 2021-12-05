4a6c87a479b91eeeb19e1a5cc6b43cb2bf45a6d5
val2703 <valerie27>
2021-12-05 17:42:59 +0100

    changement de nom bases de données projet
    changement de nom

--- SchiBd.sql
+++ SchiBd.sql
@@ -1,4 +1,5 @@
-USE schiBD
+use Schiprojet
+
 go
 
 CREATE TABLE adhérent(
@@ -6,52 +7,46 @@ CREATE TABLE adhérent(
    nom VARCHAR(50)  NOT NULL,
    prénom VARCHAR(50)  NOT NULL,
    pseudo VARCHAR(50)  NOT NULL,
-   date_de_naissance VARCHAR(50)  NOT NULL,
-   mot_de_passe DECIMAL(6,0)   NOT NULL,
-   adresse_mail VARCHAR(50)  NOT NULL,
-   PRIMARY KEY(Id_adhérent)
-);
-
-CREATE TABLE formulaire(
-   Id_formulaire INT IDENTITY,
-   nom VARCHAR(50)  NOT NULL,
-   prénom VARCHAR(50)  NOT NULL,
-   pseudo VARCHAR(50)  NOT NULL,
    date_de_naissance DATE NOT NULL,
-   mot_de_passe VARCHAR(50)  NOT NULL,
-   Id_adhérent INT NOT NULL,
-   PRIMARY KEY(Id_formulaire),
-   FOREIGN KEY(Id_adhérent) REFERENCES adhérent(Id_adhérent)
+   mot__passe DECIMAL(6,0)   NOT NULL,
+   adresse_mail VARCHAR(50)  NOT NULL,
+   Type_ad VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_adhérent)
 );
 
 CREATE TABLE forum_de_témoignages(
    Id_forum_de_témoignages INT IDENTITY,
-   adhérents VARCHAR(50)  NOT NULL,
-   des_themes VARCHAR(50)  NOT NULL,
-   date_dépôt_du_témoignage DATE NOT NULL,
-   témoignages VARCHAR(280)  NOT NULL,
+   Pseudo VARCHAR(50)  NOT NULL,
+   Mot_passe DECIMAL(6,0)   NOT NULL,
+   Date_dépot DATE NOT NULL,
+   Titre VARCHAR(50)  NOT NULL,
+   Témoignage VARCHAR(380)  NOT NULL,
+   Valide BIT NOT NULL,
+   Banni BIT NOT NULL,
+   Date_ajout DATE NOT NULL,
    PRIMARY KEY(Id_forum_de_témoignages)
 );
 
 CREATE TABLE Forum_de_discussions(
    Id_Forum_de_discussions INT IDENTITY,
-   un_theme VARCHAR(50)  NOT NULL,
-   des_adhérents VARCHAR(50)  NOT NULL,
-   médiateur_s_ VARCHAR(50)  NOT NULL,
-   des_questions VARCHAR(200)  NOT NULL,
-   des_réponses VARCHAR(200)  NOT NULL,
-   des_catégories VARCHAR(110)  NOT NULL,
-   Date_de_la_question DATE NOT NULL,
-   Date_de_la_réponse DATE NOT NULL,
+   Pseudo VARCHAR(50)  NOT NULL,
+   Mot_passe VARCHAR(50) ,
+   Catégorie VARCHAR(50)  NOT NULL,
+   Thème VARCHAR(50)  NOT NULL,
+   Question VARCHAR(200)  NOT NULL,
+   Réponse VARCHAR(200)  NOT NULL,
+   Date_question DATE NOT NULL,
+   Date__réponse DATE NOT NULL,
+   Valide BIT NOT NULL,
+   Banni BIT NOT NULL,
    PRIMARY KEY(Id_Forum_de_discussions)
 );
 
 CREATE TABLE modérateur(
    Id_modérateur INT IDENTITY,
-   adresse_mail_adhérent VARCHAR(50)  NOT NULL,
    Pseudo VARCHAR(50) ,
-   question_réponse VARCHAR(50)  NOT NULL,
-   article VARCHAR(50)  NOT NULL,
+   Mot_passe DECIMAL(6,0)   NOT NULL,
+   Adresse_mail VARCHAR(50)  NOT NULL,
    PRIMARY KEY(Id_modérateur)
 );
 
@@ -59,7 +54,6 @@ CREATE TABLE Lexique_abécedaire(
    Id_Lexique_abécedaire INT IDENTITY,
    mots VARCHAR(50) ,
    lettres VARCHAR(1) ,
-   Définition_des_mots VARCHAR(50) ,
    PRIMARY KEY(Id_Lexique_abécedaire)
 );
 
@@ -67,6 +61,26 @@ CREATE TABLE Labell(
    Id_Labell INT IDENTITY,
    Définition VARCHAR(50) ,
    PRIMARY KEY(Id_Labell)
+);
+
+CREATE TABLE Visiteur(
+   Id_Visiteur INT IDENTITY,
+   PRIMARY KEY(Id_Visiteur)
+);
+
+CREATE TABLE video(
+   Id_video INT IDENTITY,
+   intitulé VARCHAR(50) ,
+   lien_video VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_video)
+);
+
+CREATE TABLE image(
+   Id_image INT IDENTITY,
+   intitulé VARCHAR(50)  NOT NULL,
+   lien_image VARCHAR(50)  NOT NULL,
+   position_image VARCHAR(50)  NOT NULL,
+   PRIMARY KEY(Id_image)
 );
 
 CREATE TABLE participer(
@@ -80,7 +94,6 @@ CREATE TABLE participer(
 CREATE TABLE Vérifier(
    Id_forum_de_témoignages INT,
    Id_modérateur INT,
-   valider BIT,
    PRIMARY KEY(Id_forum_de_témoignages, Id_modérateur),
    FOREIGN KEY(Id_forum_de_témoignages) REFERENCES forum_de_témoignages(Id_forum_de_témoignages),
    FOREIGN KEY(Id_modérateur) REFERENCES modérateur(Id_modérateur)
@@ -97,18 +110,41 @@ CREATE TABLE déposer(
 CREATE TABLE Contrôler(
    Id_Forum_de_discussions INT,
    Id_modérateur INT,
-   valider BIT NOT NULL,
-   bannir BIT,
    PRIMARY KEY(Id_Forum_de_discussions, Id_modérateur),
    FOREIGN KEY(Id_Forum_de_discussions) REFERENCES Forum_de_discussions(Id_Forum_de_discussions),
    FOREIGN KEY(Id_modérateur) REFERENCES modérateur(Id_modérateur)
 );
 
-CREATE TABLE Asso_7(
+CREATE TABLE contenir(
    Id_Lexique_abécedaire INT,
    Id_Labell INT,
    PRIMARY KEY(Id_Lexique_abécedaire, Id_Labell),
    FOREIGN KEY(Id_Lexique_abécedaire) REFERENCES Lexique_abécedaire(Id_Lexique_abécedaire),
    FOREIGN KEY(Id_Labell) REFERENCES Labell(Id_Labell)
 );
+
+CREATE TABLE rechercher(
+   Id_Lexique_abécedaire INT,
+   Id_Visiteur INT,
+   PRIMARY KEY(Id_Lexique_abécedaire, Id_Visiteur),
+   FOREIGN KEY(Id_Lexique_abécedaire) REFERENCES Lexique_abécedaire(Id_Lexique_abécedaire),
+   FOREIGN KEY(Id_Visiteur) REFERENCES Visiteur(Id_Visiteur)
+);
+
+CREATE TABLE regerder(
+   Id_Visiteur INT,
+   Id_video INT,
+   PRIMARY KEY(Id_Visiteur, Id_video),
+   FOREIGN KEY(Id_Visiteur) REFERENCES Visiteur(Id_Visiteur),
+   FOREIGN KEY(Id_video) REFERENCES video(Id_video)
+);
+
+CREATE TABLE voir(
+   Id_Visiteur INT,
+   Id_image INT,
+   PRIMARY KEY(Id_Visiteur, Id_image),
+   FOREIGN KEY(Id_Visiteur) REFERENCES Visiteur(Id_Visiteur),
+   FOREIGN KEY(Id_image) REFERENCES image(Id_image)
+);
+
 go
